a4e0cc5f935fa790a194efabea574fac8d1b71f2
Kaique Ferreira <103851363+KaiqueDev1@users.noreply.github.com>
2025-01-10 19:22:12 -0300

    Install shadcnui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,27 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "#ced4da",
-        primary: "rgb(242,116,149)",
-        secundary: "rgb(191,210,228)",
-        text: "rgba(0, 0, 0, 0.87)",
-        shadow: "rgba(0, 0, 0, 0.12)",
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: '#ced4da',
+  			primary: 'rgb(242,116,149)',
+  			secundary: 'rgb(191,210,228)',
+  			text: 'rgba(0, 0, 0, 0.87)',
+  			shadow: 'rgba(0, 0, 0, 0.12)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
