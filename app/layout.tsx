80254ec0eca0fb4cd48ce5e890d74ee3a1c0028e
KaiqueDev1 <kakamelo141205@gmail.com>
2025-01-10 15:57:18 -0300

    Initial project candy

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,9 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt_BR">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-background antialiased`}
       >
         {children}
       </body>
