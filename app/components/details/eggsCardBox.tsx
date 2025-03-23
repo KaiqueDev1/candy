53fe50bafe23e074ee1aa69dddfc468a5890be66
Kaique Ferreira <103851363+KaiqueDev1@users.noreply.github.com>
2025-03-22 21:10:39 -0300

    FEAT: New icon of close

--- app/components/details/eggsCardBox.tsx
+++ app/components/details/eggsCardBox.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
 import Image, { StaticImageData } from "next/image";
 import Order from "../Contact/order";
+import { MoveLeftIcon } from "lucide-react";
 interface OrderProps {
   egg: {
     id: string;
@@ -23,13 +22,8 @@ const EggsCardBox: React.FC<OrderProps> = ({ egg, onClose }) => {
   return (
     <div className="fixed inset-0 z-50 flex h-full items-center justify-center bg-background">
       <Card className="h-full w-full rounded-none border-none md:w-[600px]">
-        <Button
-          variant="ghost"
-          onClick={onClose}
-          className="absolute left-4 top-4 z-50 bg-shadow text-text"
-        >
-          <X size={30} />
-        </Button>
+        <MoveLeftIcon size={30} onClick={onClose} className="m-2" />
+
         <CardContent className="flex h-full flex-col gap-4 overflow-y-auto p-0">
           <Image
             src={egg.image}
@@ -37,37 +31,36 @@ const EggsCardBox: React.FC<OrderProps> = ({ egg, onClose }) => {
             width={460}
             height={360}
             draggable={false}
-            className="rounded-b-3xl"
+            className="rounded-3xl p-2"
           />
           <div className="mt-0 flex h-full w-full gap-9 px-1">
             <div className="mt-0 flex w-full flex-col gap-3 px-4">
-              <div className="flex flex-col gap-4">
-                <CardTitle className="rounded-xl font-inter text-2xl font-medium text-text">
+              <div className="mb-4 flex items-center justify-between gap-4">
+                <CardTitle className="rounded-xl font-inter text-2xl font-semibold text-text">
                   {egg.name}
                 </CardTitle>
+                <h3 className="flex gap-2 font-playwrite text-base font-bold text-primary">
+                  R${egg.price}
+                </h3>
               </div>
 
               <div className="flex flex-col">
-                <h3 className="font-inter text-base font-semibold text-primary">
-                  Descrição:
-                </h3>
-                <p className="text-lg font-light text-text">
+                <p className="font-poppins text-lg font-normal text-text">
                   {egg.description}
                 </p>
               </div>
               <div className="flex items-center justify-evenly">
-                <p className="select-none rounded-md bg-background px-3 py-1 text-lg text-text">
+                <p className="select-none rounded-full bg-secundary px-5 py-2 font-poppins text-base font-medium text-primary">
                   {egg.grams}
                 </p>
-                <p className="select-none rounded-md bg-background px-3 py-1 text-lg text-text">
+                <p className="select-none rounded-full bg-secundary px-5 py-2 font-poppins text-base font-medium text-primary">
                   {egg.chocolate}
                 </p>
+                <p className="select-none rounded-full bg-secundary px-5 py-2 font-poppins text-base font-medium text-primary">
+                  {egg.type}
+                </p>
               </div>
-              <div className="py-3">
-                <h3 className="flex gap-2 text-xl font-normal text-text">
-                  R${egg.price}
-                </h3>
-              </div>
+              <div className="py-3"></div>
               {
                 <Order
                   egg={{
