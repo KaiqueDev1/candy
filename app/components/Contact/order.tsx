64e94dde764c4c7a166bbaa1f783ccd0de0c3cb4
Kaique Ferreira <103851363+KaiqueDev1@users.noreply.github.com>
2025-03-22 21:11:07 -0300

    FEAT: New button of instagram and icon "View"

--- app/components/Contact/order.tsx
+++ app/components/Contact/order.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Send } from "lucide-react";
+import { Eye, Send } from "lucide-react";
 import { StaticImageData } from "next/image";
 
 interface EggsCardBoxProps {
@@ -12,7 +12,6 @@ interface EggsCardBoxProps {
     ingredients: string;
     type: string;
     image: StaticImageData;
-    
   };
 }
 
@@ -23,15 +22,26 @@ const Order: React.FC<EggsCardBoxProps> = ({ egg }) => {
     const url = `https://api.whatsapp.com/send?phone=5511919462746&text=${encodedMessage}`;
     window.open(url, "_blank");
   };
+  const handleInstagramClick = () => {
+    const url = `https://www.instagram.com/kah_candylover/`;
+    window.open(url, "_blank");
+  };
 
   return (
-    <section className="flex gap-4 px-4">
+    <section className="flex gap-2">
       <Button
         className="flex w-full items-center justify-center bg-whatsapp font-inter text-base font-semibold text-secundary hover:text-text"
         onClick={handleWhatsAppClick}
       >
         <Send size={20} className="mr-2" />
         Encomendar
+      </Button>
+      <Button
+        className="flex w-full items-center justify-center bg-instagram font-inter text-base font-semibold text-secundary hover:text-text"
+        onClick={handleInstagramClick}
+      >
+        <Eye size={20} className="mr-2" />
+        Ver mais
       </Button>
     </section>
   );
